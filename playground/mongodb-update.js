f1100cafc617055a8358ed61fe2eb2ff3ab8c58a
Ragan Martinez <ragan.martinez@live.com>
2018-10-26 11:01:35 +1100

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,54 @@
+// const MongoClient = require("mongodb").MongoClient;
+const { MongoClient, ObjectID } = require("mongodb");
+
+MongoClient.connect(
+  "mongodb://localhost:27017/TodoApp",
+  (err, db) => {
+    if (err) {
+      return console.log("Unable to connect to MongoDB server");
+    }
+    console.log("Connected to MongoDB server");
+
+    // findOneAndUpdate
+    // db.collection("Todos")
+    //   .findOneAndUpdate(
+    //     {
+    //       _id: new ObjectID("5bd25196857caf484db4e8cf")
+    //     },
+    //     {
+    //       $set: {
+    //         completed: true
+    //       }
+    //     },
+    //     {
+    //       returnOriginal: false
+    //     }
+    //   )
+    //   .then(result => {
+    //     console.log(result);
+    //   });
+
+    db.collection("Users")
+      .findOneAndUpdate(
+        {
+          _id: new ObjectID("5bd2472a19001c4774175047")
+        },
+        {
+          $set: {
+            name: "Ragan"
+          },
+          $inc: {
+            age: 1
+          }
+        },
+        {
+          returnOriginal: false
+        }
+      )
+      .then(result => {
+        console.log(JSON.stringify(result, undefined, 2));
+      });
+
+    // db.close();
+  }
+);
